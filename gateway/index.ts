ac85c00d110748f5de4f9d3ac79f5c2835003aef
GabrielAndresCruz <andrescruz048@hotmail.com>
2023-09-17 12:04:56 -0300

    New routes on gateway

--- gateway/index.ts
+++ gateway/index.ts
@@ -1,11 +1,6 @@
 import express, { Express } from "express";
 import morgan from "morgan";
-import {
-  createProxyMiddleware,
-  Filter,
-  Options,
-  RequestHandler,
-} from "http-proxy-middleware";
+import { createProxyMiddleware } from "http-proxy-middleware";
 
 const PORT = 8000;
 
@@ -21,6 +16,22 @@ app.use(
   })
 );
 
+app.use(
+  "/films",
+  createProxyMiddleware({
+    target: "http://localhost:8002",
+    changeOrigin: true,
+  })
+);
+
+app.use(
+  "/planets",
+  createProxyMiddleware({
+    target: "http://localhost:8003",
+    changeOrigin: true,
+  })
+);
+
 app.listen(PORT, () => {
-  console.log(`Gateway on port ${8000}`);
+  console.log(`Gateway on port ${PORT}`);
 });
