ff5e1c1b91ca7153550beb3631ed3d9ca7ef7c3f
GabrielAndresCruz <andrescruz048@hotmail.com>
2023-09-19 13:05:34 -0300

    Character Schema created

---
+++ database/src/database/schemas/characterSchema.ts
@@ -0,0 +1,57 @@
+import { InferSchemaType, Schema, Types, model } from "mongoose";
+
+const charactersSchema = new Schema({
+  _id: {
+    type: Types.ObjectId,
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  height: {
+    type: String,
+    required: true,
+  },
+  mass: {
+    type: String,
+    required: true,
+  },
+  hair_color: {
+    type: String,
+    required: true,
+  },
+  skin_color: {
+    type: String,
+    required: true,
+  },
+  eye_color: {
+    type: String,
+    required: true,
+  },
+  birth_year: {
+    type: String,
+    required: true,
+  },
+  gender: {
+    type: String,
+    enum: ["male", "female", "hermaphrodite", "none", "n/a"],
+    required: true,
+  },
+  homeworld: {
+    type: String,
+    required: true,
+    ref: "Planet",
+  },
+  films: [
+    {
+      type: String,
+      required: true,
+      ref: "Film",
+    },
+  ],
+});
+
+type Character = InferSchemaType<typeof charactersSchema>;
+
+export default model<Character>("Character", charactersSchema);
