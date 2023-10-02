7852c856fa58bf17b0aa209fa4822c2afd5af521
GabrielAndresCruz <andrescruz048@hotmail.com>
2023-10-02 11:47:36 -0300

    Changing local route to producte route (docker proxy)

--- characters/src/data/index.ts
+++ characters/src/data/index.ts
@@ -6,23 +6,23 @@ async function listCharacters() {
 }
 
 async function listOneCharacter(id: string) {
-  const characters = await axios.get(`http://localhost:8004/character/${id}`);
+  const characters = await axios.get(`http://database:8004/character/${id}`);
   return characters.data;
 }
 
 async function createCharacter(body: any[]) {
-  const character = await axios.post(`http://localhost:8004/character/`, body);
+  const character = await axios.post(`http://database:8004/character/`, body);
   return character.data;
 }
 
 async function deleteCharacter(id: string) {
-  const character = await axios.delete(`http://localhost:8004/character/${id}`);
+  const character = await axios.delete(`http://database:8004/character/${id}`);
   return character.data;
 }
 
 async function updateCharacter(id: string, body: any[]) {
   const character = await axios.put(
-    `http://localhost:8004/character/${id}`,
+    `http://database:8004/character/${id}`,
     body
   );
   return character.data;
--- films/src/data/index.ts
+++ films/src/data/index.ts
@@ -1,27 +1,27 @@
 import axios from "axios";
 
 async function listFilms() {
-  const films = await axios.get("http://localhost:8004/film");
+  const films = await axios.get("http://database:8004/film");
   return films.data;
 }
 
 async function listOneFilm(id: String) {
-  const film = await axios.get(`http://localhost:8004/film/${id}`);
+  const film = await axios.get(`http://database:8004/film/${id}`);
   return film.data;
 }
 
 async function createFilm(body: any[]) {
-  const film = await axios.post(`http://localhost:8004/film/`, body);
+  const film = await axios.post(`http://database:8004/film/`, body);
   return film.data;
 }
 
 async function deleteFilm(id: string) {
-  const film = await axios.delete(`http://localhost:8004/film/${id}`);
+  const film = await axios.delete(`http://database:8004/film/${id}`);
   return film.data;
 }
 
 async function updateFilm(id: string, body: any[]) {
-  const film = await axios.put(`http://localhost:8004/film/${id}`, body);
+  const film = await axios.put(`http://database:8004/film/${id}`, body);
   return film.data;
 }
 
