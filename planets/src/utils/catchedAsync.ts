ab9572f580c5b686c41f982e60148f167e0ef9cc
GabrielAndresCruz <andrescruz048@hotmail.com>
2023-10-07 10:49:57 -0300

    Putting new error object for response in microservices

--- planets/src/utils/catchedAsync.ts
+++ planets/src/utils/catchedAsync.ts
@@ -1,5 +1,18 @@
 import { NextFunction, Request, Response } from "express";
 
+const errorFormater = (err: String) => {
+  let errors = {};
+
+  const allErrors = err.substring(err.indexOf(":") + 1).trim();
+  const allErrorsArray = allErrors.split(",").map((e: String) => e.trim());
+  allErrorsArray.forEach((error) => {
+    const [key, value] = error.split(":").map((err: String) => err.trim());
+    errors[key] = value;
+  });
+
+  return errors;
+};
+
 const catchedAsync = (
   fn: (req: Request, res: Response, next: NextFunction) => Promise<any>
 ) => {
@@ -8,7 +21,7 @@ const catchedAsync = (
       res.status(500).send({
         success: false,
         message: "Internal Server Error",
-        error: err.message,
+        error: errorFormater(err.response.data.error),
       });
     });
   };
